e07be9fe2785b73d81c9c9cf20cf3ded594c39c1
ikbal26 <tadzhihodzhaevikbal@gmail.com>
2024-12-07 19:16:48 +0600

    икбал добавил файл module_ikbal.py; изменил файл main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello world")
+from module_ikbal import STUDENTS_GRADES, STUDENTS_LIST
+def show_students(students):
+    for i in students:
+        print(i)
+
+show_students(STUDENTS_LIST)
---
+++ module-ikbal.py
@@ -0,0 +1,17 @@
+STUDENT_LIST = [
+    'Esen M.',
+    'Maryam E.',
+    'Mohammad M.',
+    'Ahmad A.',
+    'Rahim R.',
+]
+
+MAX_SCORE = 30
+
+STUDENT_GRADES = (
+    (15, MAX_SCORE),
+    (12, MAX_SCORE),
+    (30, MAX_SCORE),
+    (1, MAX_SCORE),
+    (99, MAX_SCORE),
+)
